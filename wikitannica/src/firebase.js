b5db1e116abf49fd525bfa6011baaf1e438ec39b
CoderMB1138 <mbagdazian90@gmail.com>
2024-06-17 16:34:20 +0200

    fixed firebase

--- wikitannica/src/firebase.js
+++ wikitannica/src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD_jQNBmhk3pQXqsqPTeQIU5DEpmUEtjiI",
-  authDomain: "wikitannica-b1071.firebaseapp.com",
-  projectId: "wikitannica-b1071",
-  storageBucket: "wikitannica-b1071.appspot.com",
-  messagingSenderId: "201658660893",
-  appId: "1:201658660893:web:89f176ef25a5b667d26ef4",
-  measurementId: "G-JC4DYYLGSG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
